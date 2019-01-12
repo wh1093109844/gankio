9ec3044b28a555d6f7dd0a2e7d56515d6cab2819
wanghe <1093109844wanghe@gmail.com>
2019-01-12 13:26:05 +0800

    Initial Commit

---
+++ gankio/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  gankio
+//
+//  Created by za-wanghe on 2019/1/12.
+//  Copyright © 2019 za-wanghe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ gankio/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  gankio
+//
+//  Created by za-wanghe on 2019/1/12.
+//  Copyright © 2019 za-wanghe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
